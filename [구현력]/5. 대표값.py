d7f0f211248a47ecab935e8b99a63edec7aaee5f
LuckySunny <saystar1320@naver.com>
2021-10-15 01:11:01 +0900

    5. 대표값

---
+++ [구현력]/5. 대표값.py
@@ -0,0 +1,16 @@
+N = int(input())
+a = list(map(int, input().split()))
+ave=round(sum(a)/N)
+min=float('inf')
+
+for idx, x in enumerate(a):
+    tmp=abs(x-ave)
+    if tmp<min:
+        min=tmp
+        score = x
+        res = idx+1
+    elif tmp==min:
+        if x>score:
+            score = x
+            res = idx+1
+print(ave, res)
