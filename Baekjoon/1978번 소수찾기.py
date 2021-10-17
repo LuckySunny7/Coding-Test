be4acdfa31363a10ef6264d80e290a713501d2d9
LuckySunny <saystar1320@naver.com>
2021-10-17 15:38:34 +0900

    1978번 소수찾기

---
+++ Baekjoon/1978번 소수찾기.py
@@ -0,0 +1,14 @@
+N = int(input())
+a = list(map(int, input().split()))
+
+res = 0
+
+for i in a:
+    cnt = 0
+    if i >1:
+        for j in range(2,i):
+            if i%j == 0:
+                cnt+=1
+        if cnt == 0:
+            res +=1
+print(res)
