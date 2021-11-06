9b9156c0ab2a49bb85b225425e195f99438a52e5
LuckySunny <saystar1320@naver.com>
2021-11-06 23:41:21 +0900

    1929 소수 구하기

---
+++ Baekjoon/1929 소수 구하기.py
@@ -0,0 +1,16 @@
+m, n = map(int, input().split())
+
+prime_list = [True] * (n + 1)
+x = int((n + 1) ** 0.5)
+
+for i in range(2, x + 1):
+    if prime_list[i] == True:
+        for j in range(i + i, n + 1, i):
+            prime_list[j] = False
+
+
+sieve = [i for i in range(2, n + 1) if prime_list[i] == True]
+
+for i in range(len(sieve)):
+    if sieve[i] >= m:
+        print(sieve[i])
