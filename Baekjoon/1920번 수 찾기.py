d6660ddc9a826d16563ff9e50ba001de4f98b5b1
LuckySunny <saystar1320@naver.com>
2021-11-24 18:08:39 +0900

    1920번 수 찾기

---
+++ Baekjoon/1920번 수 찾기.py
@@ -0,0 +1,10 @@
+N = int(input())
+arr = list(map(int, input().split()))
+M= int(input())
+arr2 = list(map(int, input().split()))
+
+for i in arr2:
+    if i in arr:
+        print(1)
+    else:
+        print(0)
